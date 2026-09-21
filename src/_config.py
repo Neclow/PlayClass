4e721f010e7e9323a66cfcc87dca8f94bd294e6e
Neclow <neil.clow@sund.ku.dk>
2026-09-21 16:45:00 +0200

    chore: add tracking constant dir

--- src/_config.py
+++ src/_config.py
@@ -4,8 +4,10 @@ DEFAULT_DATA_DIR: Final = "data"
 DEFAULT_DATASET_DIR: Final = f"{DEFAULT_DATA_DIR}/dataset"
 DEFAULT_CHECKPOINT_DIR: Final = f"{DEFAULT_DATA_DIR}/eval"
 DEFAULT_LABEL_DIR: Final = f"{DEFAULT_DATA_DIR}/labels"
-DEFAULT_TRACKING_DIR: Final = f"{DEFAULT_DATA_DIR}/postprocessing"
-DEFAULT_VIDEO_DIR: Final = f"{DEFAULT_DATA_DIR}/video"
+DEFAULT_POSTPROCESSING_DIR: Final = f"{DEFAULT_DATA_DIR}/postprocessing"
+DEFAULT_RESULTS_DIR: Final = f"{DEFAULT_DATA_DIR}/results"
+DEFAULT_TRACKING_DIR: Final = f"{DEFAULT_RESULTS_DIR}/tracking"
+DEFAULT_VIDEO_DIR: Final = f"{DEFAULT_DATA_DIR}/videos"
 # Discard windows where less than this fraction of frames are available after postprocessing
 DEFAULT_MIN_WINDOW_COVERAGE: Final = 0.5
 DEFAULT_FPS: Final = 25.0
